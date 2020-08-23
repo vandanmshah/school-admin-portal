b4777fd7056e1f7dbb46295f7c054440dc3791c2
Vandan M. Shah <vandanshah77@gmail.com>
2020-08-23 21:59:36 +0530

    Initial commit

---
+++ src/Components/home/home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div className='Home'>Hello I am in Home</div>;
+};
+
+export default Home;
---
+++ src/Components/login/login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './login.css';
+
+const Login = () => {
+  return <div className='login'>Hello I am in login</div>;
+};
+
+export default Login;

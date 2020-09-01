7f0fae46e019a2557d924b8fdffd9bb353443996
Jinay Shah <shahjinay1996@gmail.com>
2020-09-01 15:33:29 +0530

    Add : NavBar Component

---
+++ src/Components/Common/NavBar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar: React.FC<{}> = () => {
+  return (
+    <>
+      <div className='Home'>School Management System</div>
+    </>
+  );
+};
+
+export default NavBar;
--- src/Components/home/home.tsx
+++ src/Components/home/home.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './home.css';
 
 const Home: React.FC<{}> = () => {
   return (
     <>
-      <div className='Home'>Hello I am in Home</div>;
+      <div className='Home'>Hello I am in Home</div>
     </>
   );
 };

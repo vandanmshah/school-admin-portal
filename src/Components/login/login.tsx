2660b8777ebc0735752c8a37f78cfd6b57563077
Jinay Shah <shahjinay1996@gmail.com>
2020-09-01 18:24:37 +0530

    Added : login functionality

--- src/Components/login/login.tsx
+++ src/Components/login/login.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from '../../../node_modules/axios'
 import './login.css';
+import  {API_URL}  from '../../config/dev/env'
 
 const Login: React.FC<{}> = () => {
+
+  const [loginCredentials, setloginCredentials] = useState({ email:'',password:''})
+
+  // use to update state onchange of email and password
+  const handleChange = (e:any) => {
+    const {id , value} = e.target   
+    setloginCredentials(prevState => ({
+        ...prevState,
+        [id] : value
+    }))
+}
+
+  // use for API call on submit
+ const handleSubmit = () => {
+   // API call
+   const url = API_URL + 'signin'
+   
+   axios.post(url,loginCredentials).then(res => {
+     console.log(res.data,'eeeeeee')
+   })
+ }
+
   return (
     <>
-      <div className='login'>Hello I am in login</div>
+      <div className='login'>Hello I am in login</div>  
+      <input type="email" 
+                       className="form-control" 
+                       id="email" 
+                      //  aria-describedby="emailHelp" 
+                       placeholder="Enter email" 
+                       value={loginCredentials.email}
+                       onChange={handleChange}
+                />
+    
+      <input type="password" 
+                        className="form-control" 
+                        id="password" 
+                        placeholder="Password"
+                        value={loginCredentials.password}
+                        onChange={handleChange} 
+                    />
+
+      <button type="submit" onClick={handleSubmit}> Submit </button>
+    )
     </>
   );
-};
+}
 
 export default Login;
--- src/config/dev/env.ts
+++ src/config/dev/env.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:3000/';
-export const API_URL = `${BASE_URL}/api/`;
+export const BASE_URL = 'http://localhost:3000/'
+export const API_URL = 'http://localhost:3001/api/' // change due to conflict in BE&FE same port

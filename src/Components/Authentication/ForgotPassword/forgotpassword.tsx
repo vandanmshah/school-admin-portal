57331b80fea24f9a16819b5e4d927fbe85749899
Jinay Shah <shahjinay1996@gmail.com>
2020-09-04 13:08:08 +0530

    Added : Dashboard and Forgot password module

---
+++ src/Components/Authentication/ForgotPassword/forgotpassword.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './home.css';
+
+const ForgotPassword: React.FC<{}> = () => {
+  return (
+    <>
+      <div className='Home'>Hello I am in Forgotpassword</div>;
+    </>
+  );
+};
+
+export default ForgotPassword;

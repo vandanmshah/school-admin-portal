54e8c31cc9c9ec7faa066e98d9a76fbd8d0c1b16
Jinay Shah <shahjinay1996@gmail.com>
2020-10-12 11:45:11 +0530

    Fixed : Ul issues (#12)
    * Fixed : Ul issues
    
    * fixed : css
    
    * Remove : comments

--- src/Components/Authentication/Login/login.tsx
+++ src/Components/Authentication/Login/login.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Components
 import { Grid, TextField, makeStyles, Button, Typography, Box, Link } from '@material-ui/core';
-// import avatar from '../../../Assets/jinu.jpg';
+import avatar from '../../../Assets/lifeline.jpg';
 import { useForm, Form } from '../../Common/useForm';
 
 // constants
@@ -13,8 +13,11 @@ import { env } from '../../../config';
 import './login.css';
 
 const useStyle = makeStyles(() => ({
+  loginData: {
+    margin: '10% 0% 0% 5%',
+    width: '45%',
+  },
   submit: {
-    // marginTop: theme.spacing(3),
     'backgroundColor': '#211b4e !important',
     '&:hover': {
       backgroundColor: '#151039 !important',
@@ -30,8 +33,15 @@ const useStyle = makeStyles(() => ({
     'textTransform': 'initial',
     'marginLeft': '10%',
   },
+  login: {
+    backgroundImage: `url(${avatar})`,
+    width: '100%',
+    height: '90vh',
+    marginTop: '3.5px',
+    backgroundSize: 'cover',
+    backgroundPosition: '50%',
+  },
   sideImg: {
-    // bottom: '360px',
     top: '-360px',
     left: '50%',
     width: '50%',
@@ -66,40 +76,32 @@ const Login: React.FC<{}> = () => {
 
   return (
     <>
-      <Box m={10}>
-        <span>
-          <Typography component='h5' variant='h5'>
-            Enter Login Details below...
-          </Typography>
-        </span>
-        <Form>
-          <Grid container>
-            <TextField
-              // variant='filled' // for diff varient like outline etc
-              label='Email'
-              name='email'
-              value={values.email}
-              onChange={handleInputChange}
-            />
-          </Grid>
-          <Grid container>
-            <TextField
-              // variant='outlined' // for diff varient like outline etc
-              label='Password'
-              name='password'
-              value={values.password}
-              onChange={handleInputChange}
-            />
-          </Grid>
-        </Form>
-        <Button className={classes.submit} color='primary' type='submit' variant='contained' onClick={handleSubmit}>
-          Submit
-        </Button>
-        <Link className={classes.link} href='/forgot'>
-          Forgot Password?
-        </Link>
-      </Box>
-      <img className={classes.sideImg} src={avatar} alt='' />
+      <Grid container spacing={1}>
+        <Grid className={classes.loginData}>
+          <span>
+            <Typography component='h5' variant='h5'>
+              Enter Login Details below...
+            </Typography>
+          </span>
+          <Form>
+            <Grid>
+              <TextField label='Email' name='email' value={values.email} onChange={handleInputChange} />
+            </Grid>
+            <Grid>
+              <TextField label='Password' name='password' value={values.password} onChange={handleInputChange} />
+            </Grid>
+          </Form>
+          <Button className={classes.submit} color='primary' type='submit' variant='contained' onClick={handleSubmit}>
+            Submit
+          </Button>
+          <Link className={classes.link} href='/forgot'>
+            Forgot Password?
+          </Link>
+        </Grid>
+        <Grid container sm={6} md={6} direction='column'>
+          <div className={classes.login} />
+        </Grid>
+      </Grid>
     </>
   );
 };

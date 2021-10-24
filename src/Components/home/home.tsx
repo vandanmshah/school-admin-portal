d4ea7751e4c607519b9770d551fe25c7d4874cfa
jinay shah <jinays@kuberdigital.com>
2021-10-24 22:35:35 +0530

    Changes for homepage

--- src/Components/home/home.tsx
+++ src/Components/home/home.tsx
@@ -4,7 +4,13 @@ import './home.css';
 const Home: React.FC<{}> = () => {
   return (
     <>
-      <div className='Home'>Hello I am in Home</div>;
+      <div className='Home'>
+        <h1 className='main-title'>Everything you need to manage Educational Institute.</h1>
+        <p className='main-description'>
+          Managing a school is no easy task. specially to monitor the student report. also managing admission and
+          enrollment process is quite difficult.
+        </p>
+      </div>
     </>
   );
 };
